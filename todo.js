5640c2aa912c8e48358a46517247614e756e651d
AtillaSzikora <szikora.atilla@gmail.com>
2016-05-13 10:35:16 +0200

    everything work so far

--- todo.js
+++ todo.js
@@ -4,22 +4,46 @@
     var btn = document.getElementById("addBtn");
     var todoList = document.getElementById("todos");
     
-    var makeToDoHtml = function (todoString) {
+    var makeToDoHtml = function (todoString, checkHandler) {
         var element = document.createElement("li");
         var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
         var span = document.createElement("span");
+
+        checkbox.type = "checkbox";
         span.textContent = todoString;
         element.appendChild(checkbox);
         element.appendChild(span);
-        return element; };
+        checkbox.addEventListener("click", checkHandler);
 
-    var onChecked = function (event) {
-        
+        return element;
     };
 
-    todoList.appendChild(makeToDoHtml("Item 1"));
-    todoList.appendChild(makeToDoHtml("Item 2"));
-    todoList.appendChild(makeToDoHtml("Item 3"));
+    var onChecked = function (event) {
+        var target = event.target;
+        if (target.checked) {
+            target.parentElement.classList.add("done"); }
+        else {
+            target.parentElement.classList.remove("done"); }
+    };
+
+    var addButtonClicked = function (event) {
+        var todoText = text.value;
+        todoList.appendChild(makeToDoHtml(todoText, onChecked));
+        text.value = "";
+        text.focus();
+    };
+
+    btn.addEventListener("click", addButtonClicked);
+    function inputKeyup(event) {
+        if (event.keyCode == 13) {
+            btn.click();}
+    }
+
+    text.addEventListener("keyup", inputKeyup);
+    text.focus();
+
+    todoList.appendChild(makeToDoHtml("Item 1", onChecked));
+    todoList.appendChild(makeToDoHtml("Item 2", onChecked));
+    todoList.appendChild(makeToDoHtml("Item 3", onChecked));
 
 }) ();
