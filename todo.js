dc491adc170f024fce2443bf96f141745e129e77
AtillaSzikora <szikora.atilla@gmail.com>
2016-05-13 09:57:58 +0200

    checkboxes added

--- todo.js
+++ todo.js
@@ -1,15 +1,25 @@
 (function () {
+    "use strict";
     var text = document.getElementById("todoInput");
     var btn = document.getElementById("addBtn");
     var todoList = document.getElementById("todos");
     
     var makeToDoHtml = function (todoString) {
         var element = document.createElement("li");
-        element.textContent = todoString;
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        var span = document.createElement("span");
+        span.textContent = todoString;
+        element.appendChild(checkbox);
+        element.appendChild(span);
         return element; };
+
+    var onChecked = function (event) {
+        
+    };
 
     todoList.appendChild(makeToDoHtml("Item 1"));
     todoList.appendChild(makeToDoHtml("Item 2"));
     todoList.appendChild(makeToDoHtml("Item 3"));
 
-}) ();  
+}) ();
